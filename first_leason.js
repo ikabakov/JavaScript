b25bfc1cf1b193ed4bab38eab4d9c33c65966dce
Ivo Kabakov <irkivok@gmail.com>
2016-11-13 17:33:02 +0200

    fix conditional statemens

--- first_leason.js
+++ first_leason.js
@@ -8,7 +8,7 @@ function timerfunc() {
     var min = now.getMinutes();
     var sec = now.getSeconds();
     document.getElementById("clock").value =
-       hour + ":" + min + ":" + sec;
+        hour + ":" + min + ":" + sec;
 
 }
 
@@ -21,10 +21,51 @@ function getBiggerNumber() {
 
     var TheBiggerNumber = secondNumber;
 
-    if (firstNumber>secondNumber){
+    if (firstNumber > secondNumber) {
         TheBiggerNumber = firstNumber
     }
 
     var resultTB = document.getElementById("result-tb");
     resultTB.value = "The biggest number is:" + TheBiggerNumber;
+}
+
+//switch- case: при повече от 3 случая
+
+function display_case() {
+    var num = document.getElementById('number').value;
+    var day = "You select day Invalid Weekday";
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", " Friday", "Saturday"];
+    // switch (parseInt(num)) {
+    //
+    //     case 1:
+    //         day = "Sunday";
+    //         break;
+    //
+    //     case 2:
+    //         day = "Monday";
+    //         break;
+    //
+    //     case 3:
+    //         day = "Tuesday";
+    //         break;
+    //
+    //     case 4:
+    //         day = "Wednesday";
+    //         break;
+    //
+    //     case 5:
+    //         day = "Thusday";
+    //         break;
+    //
+    //     case 6:
+    //         day = " Friday";
+    //         break;
+    //
+    //     case 7:
+    //         day = "Saturday";
+    //         break;
+    //
+    // }
+    day = num > 0 && num <= 7 ? days[num - 1] : day
+    document.getElementById("result").innerHTML = day;
 }
